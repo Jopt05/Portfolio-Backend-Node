8928e07fab1f214d1fd2921d2546bb3b9c679e5f
Jopt05 <jopt98@gmail.com>
2023-08-09 15:59:49 -0600

    Se modificó git ignore y npm start

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const server_1 = require("./model/server");
-const server = new server_1.default;
-server.listem();
