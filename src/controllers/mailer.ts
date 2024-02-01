f566c46fc7c0fbd91e7e3b57dc431e4a08c58ed1
Jopt05 <jopt98@gmail.com>
2024-02-01 17:01:44 -0600

    Catch para email

--- src/controllers/mailer.ts
+++ src/controllers/mailer.ts
@@ -3,26 +3,33 @@ import { NodeMailer } from "../model/nodemailer";
 
 export async function mailerPost( req: Request, res: Response ) {
 
-    const mailer = new NodeMailer();
+    try {
+        const mailer = new NodeMailer();
 
-    const {
-        name,
-        email,
-        message 
-    } = req.body;
+        const {
+            name,
+            email,
+            message 
+        } = req.body;
 
-    const fueEnviado = await mailer.sendEmail(
-        `${email} - ${name}`,
-        message
-    );
+        const fueEnviado = await mailer.sendEmail(
+            `${email} - ${name}`,
+            message
+        );
 
-    if( !fueEnviado ) {
+        if( !fueEnviado ) {
+            return res.status(200).json({
+                ok: false
+            })
+        }
+
         return res.status(200).json({
+            ok: true
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
             ok: false
         })
     }
-
-    return res.status(200).json({
-        ok: true
-    })
 };
